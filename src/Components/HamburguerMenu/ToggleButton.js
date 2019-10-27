04152064390697ce558531c99d8e20ce00d14117
Geraldyn Chirinos <geraldynchidesousa@gmail.com>
2019-10-27 00:28:51 -0300

    hamburguer button con animacion solo son styled components y un solo <Bar /> agregando before y after para crearlo

--- src/Components/HamburguerMenu/ToggleButton.js
+++ src/Components/HamburguerMenu/ToggleButton.js
@@ -8,8 +8,6 @@ const ToggleButton = () => {
   return (
     <Container className="container" onClick={handleClick}>
       <Bar change={closeButton} />
-      <Bar change={closeButton} />
-      <Bar change={closeButton} />
     </Container>
   )
 }
@@ -18,28 +16,53 @@ const Container = styled.div`
   cursor:pointer;
 `
 
+const styles = css`
+  width: 35px;
+  height: 5px;
+  border-radius: 5px;
+  background-color: white;
+  transition: all 0.4s;
+`
+
 const Bar = styled.div`
-    width: 35px;
-    height: 5px;
-    margin: 6px 0;
-    border-radius: 5px;
-    background-color: white;
-    transition: 0.4s;
-    :first-child{
+    ${styles};
+    position: relative;
+    ${props => props.change && css`
+      background-color: transparent;
+    `}
+    &:before{
+      content: '';
+      ${styles};
+      position: absolute;
+      top: -10px;
+      ${props => props.change && css`
+        transform: rotate(-45deg)  translate(-11px, 4px);
+      `};
+    }
+     &:after{
+       content: '';
+       ${styles};
+      position: absolute;
+      bottom: -10px;
+      ${props => props.change && css`
+        transform: rotate(45deg) translate(-10px, -5px)
+      `};
+    }
+`
+
+/*
+ :first-child{
       ${props => props.change && css`
         transform: rotate(-45deg)  translate(-9px, 6px)
       `};
     }
     :nth-child(2){
-      ${props => props.change && css`
-        opacity: 0;
-      `}
+
     }
     :last-child{
       ${props => props.change && css`
         transform: rotate(45deg) translate(-8px, -8px)
       `};
     }
-`
-
+* */
 export default ToggleButton
